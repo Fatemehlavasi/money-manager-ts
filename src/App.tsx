2d612a40021717443011597ca6ae14152bbe7a22
FatemehLavasi <111527593+Fatemehlavasi@users.noreply.github.com>
2022-11-23 12:06:52 -0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {ThemeProvider} from "@mui/material";
+import {RouterProvider} from "react-router-dom";
+import {Route} from "./router/route";
+import {theme} from "./theme/theme";
+import {Provider} from 'react-redux'
+import {store} from "./redux/store";
+
+function App() {
+    return (
+        <Provider store={store}>
+            <ThemeProvider theme={theme}>
+                <RouterProvider router={Route}/>
+            </ThemeProvider>
+        </Provider>
+    )
+}
+
+export default App;
